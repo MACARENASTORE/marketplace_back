55cf91544d6d5c6985530ba9b86e1749a78f0c35
MACARENASTORE <oclusiva9204@gmail.com>
2024-09-14 10:25:27 -0500

    eliminado cloudinary

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,26 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-
 // Importar los controladores de rutas
 const authRoutes = require('./authRoutes');
 const productRoutes = require('./productRoutes');
 const orderRoutes = require('./orderRoutes');
 const reviewRoutes = require('./reviewRoutes');
 const categoryRoutes = require('./categoryRoutes');
-const productRoutes1 = require('./productRoutesV1');
 
 
-// Importar el middleware de autenticación (si es necesario para algunas rutas)
+// Importar el middleware de autenticación
 const authMiddleware = require('../middleware/authMiddleware');
 
 // Definir las rutas para la API
-router.use('/auth', authRoutes);              // Rutas de autenticación
-router.use('/products', productRoutes);       // Rutas para productos
-router.use('/category', categoryRoutes); 
-router.use('/orders', authMiddleware, orderRoutes);  // Rutas de órdenes, protegidas con autenticación
-router.use('/reviews', reviewRoutes);         // Rutas para reviews o comentarios de productos
+router.use('/auth', authRoutes);               // Rutas de autenticación
+router.use('/products', productRoutes);        // Rutas para productos
+router.use('/category', categoryRoutes);       // Rutas para categorías
+router.use('/orders', authMiddleware, orderRoutes);  // Rutas de órdenes (protegidas)
+router.use('/reviews', reviewRoutes);          // Rutas para comentarios/reviews
+   
 
-router.use('/products1', productRoutes1); 
-// Exportar el router principal
 module.exports = router;
